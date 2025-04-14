d9f4767390fa6d9d0f5cfb83e52a9660136f5b4d
ass3027 <blackfeather227@gmail.com>
2025-04-14 16:44:13 +0900

    [CHORE] host 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    host: '0.0.0.0',
+  },
   plugins: [
     vue(),
     vueDevTools(),
