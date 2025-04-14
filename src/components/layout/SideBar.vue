411ccfecd1edde2ff46c68ce6a5769140134e282
ass3027 <blackfeather227@gmail.com>
2025-04-14 16:59:18 +0900

    [REFACTOR] SideBar nav font change

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -8,9 +8,9 @@ const showRealTimeView = ref(false);
 <template>
   <div class="flex flex-col w-full h-full">
     <nav class="flex flex-col">
-      <router-link to="/db-data-list">DBDataList</router-link>
-      <router-link to="/connection-fail-list">ConnectionFailList</router-link>
-      <router-link to="" @click="showRealTimeView = true">Open RealTime View</router-link>
+      <router-link to="/db-data-list" class="text-xl font-bold">DBDataList</router-link>
+      <router-link to="/connection-fail-list" class="text-xl font-bold">ConnectionFailList</router-link>
+      <router-link to="" @click="showRealTimeView = true" class="text-xl font-bold">Open RealTime View</router-link>
     </nav>
     <RealTimeView v-if="showRealTimeView"  :show="showRealTimeView" @close="showRealTimeView = false" />
   </div>
