ed75469689e0fce3f7a301d397abc9e2f5455e68
ass3027 <blackfeather227@gmail.com>
2025-04-16 11:32:25 +0900

    [REFACTOR] region name 기본 값 추가, TODO

--- src/stores/region.js
+++ src/stores/region.js
@@ -1,10 +1,11 @@
 import {defineStore} from "pinia";
 import { ref } from "vue";
 
+//TODO localStorage 써서 새로고침시에도 유지되게 하자 getter 사용
 export const useRegionStore = defineStore('region', () => {
     const currentRegion = ref({
         id: "gyeongnam",
-        name: "",
+        name: "경남",
         user: "",
         pw: "",
         ip: "",
