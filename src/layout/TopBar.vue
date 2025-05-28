7c647c1cc063f236b0958ec46bde5b8e879b6613
ass3027 <blackfeather227@gmail.com>
2025-05-28 10:51:04 +0900

    [FEATURE] 상단바

--- src/layout/TopBar.vue
+++ src/layout/TopBar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+"use strict";
 const links = [
   {
     label: 'DBDataList',
@@ -15,32 +16,34 @@ const links = [
     icon: 'pi-video',
     route: '/realtime-view'
   }
-]
+];
 </script>
 
 <template>
-<!--  <div class="flex justify-between p-4">-->
-<!--    <div>-->
-<!--      <img src="@/assets/fw-logo.svg" alt="">-->
-
-<!--    </div>-->
-<!--    <div>-->
-<!--    </div>-->
-<!--  </div>-->
   <Menubar :model="links">
     <template #start>
-      <img src="@/assets/fw-logo.svg" alt="">
+      <img class="mr-8" src="@/assets/fw-logo.svg" alt="">
     </template>
-    <template #item="{ item, props, root}">
+    <template #item="{ item, props }">
       <router-link :to="item.route" v-slot="{ href, navigate }">
-        <a class="flex items-center" v-bind="props.action">
+        <a class="flex items-center mx-4" v-bind="props.action">
           <span class="pi color-surface-800" :class="item.icon"/>
-          <span>{{ item.label}}</span>
+          <span>{{ item.label }}</span>
         </a>
       </router-link>
     </template>
     <template #end>
-
+<!--      <div class="gap-2 flex-row flex">-->
+<!--        <div class="p-2 bg-surface-200 rounded-xl">-->
+<!--          <span>경남</span>-->
+<!--        </div>-->
+<!--        <div class="p-2 bg-surface-200 rounded-xl">-->
+<!--          <span>중계 서버: {{"O"}}</span>-->
+<!--        </div>-->
+<!--        <div class="p-2 bg-surface-200 rounded-xl">-->
+<!--          <span>분석 서버: {{3}}</span>-->
+<!--        </div>-->
+<!--      </div>-->
     </template>
   </Menubar>
 </template>
