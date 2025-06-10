eaaad23dfc49b41567d5c61dea886f56400975af
ass3027 <blackfeather227@gmail.com>
2025-06-10 16:42:42 +0900

    [CSS] 상단바 크기 반응형

--- src/views/layout/MainLayout.vue
+++ src/views/layout/MainLayout.vue
@@ -5,7 +5,7 @@ import TopBar from "@/views/layout/TopBar.vue";
 
 <template>
   <div class="w-full h-full flex flex-col px-64 py-8">
-    <TopBar class="mb-8"/>
+    <TopBar class="h-1/16 mb-8"/>
     <router-view class="flex-1 min-h-0"/>
   </div>
 </template>
