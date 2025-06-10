13882430e65522b924d842fcf7e568824e8f3de1
ass3027 <blackfeather227@gmail.com>
2025-06-10 13:36:39 +0900

    [FEATURE] logType 에 따른 옵션 변경

--- src/views/CctvLog.vue
+++ src/views/CctvLog.vue
@@ -12,15 +12,18 @@ onMounted(async () => {
   })).data.message
 })
 
+// noinspection JSUnusedGlobalSymbols
 const LOG_TYPE = {
   GSTREAMER: {
     name: "GSTREAMER",
     url: "/gstreamer_log",
+    getInferenceId: _ => log.option.inferenceId,
     getFindWord: _ => "Connect failed list"
   },
   FFMPEG: {
     name: "FFMPEG",
     url: "/ffmpeg_log",
+    getInferenceId: _ => log.option.targetCctv.inference_id,
     getFindWord: _ => log.option.targetCctv.url
   }
 };
@@ -30,6 +33,7 @@ const log = reactive({
   option: {
     logType: LOG_TYPE.GSTREAMER,
     targetCctv: undefined,
+    inferenceId: "",
     date: getNow(),
   },
   fetch: {
@@ -43,17 +47,25 @@ const log = reactive({
       log.data = res.data.message;
     },
     getParam(){
+      const logType = log.option.logType
       return {
-        inference_id: log.option.targetCctv.inference_id,
+        inference_id: logType.getInferenceId(),
         date: log.option.date,
-        find_word: log.option.logType.getFindWord(),
+        find_word: logType.getFindWord(),
       };
     },
     validateOption(){
-      if(log.option.targetCctv === undefined)
-        return {status: false, msg: "CCTV를 선택 해주세요."}
+      if(log.option.logType.name === LOG_TYPE.GSTREAMER.name)
+        if(log.option.inferenceId === "")
+          return {status: false, msg: "InferenceId를 선택 해주세요."};
+
+      if(log.option.logType.name === LOG_TYPE.FFMPEG.name)
+        if(log.option.targetCctv === undefined)
+          return {status: false, msg: "CCTV를 선택 해주세요."};
+
       if(log.option.date === "")
-        return {status: false, msg: "날짜를 선택 해주세요."}
+        return {status: false, msg: "날짜를 선택 해주세요."};
+
       return  { status: true }
     },
   }
@@ -61,13 +73,18 @@ const log = reactive({
 
 const cctv = reactive({
   data: [],
+  inferenceIds: [],
   async fetch() {
     const res = await API.post("/cctv_data")
     const data = res.data.message;
-    data.forEach( it =>
-        it["L2L3"] = `${it["cctv_address"]["L2"]} ${it["cctv_address"]["L3"]}`
-    )
+
+    const inferenceIdSet = new Set();
+    data.forEach( it => {
+      it["L2L3"] = `${it["cctv_address"]["L2"]} ${it["cctv_address"]["L3"]}`
+      inferenceIdSet.add(it["inference_id"])
+    })
     this.data = data
+    this.inferenceIds = Array.from(inferenceIdSet);
   },
 })
 
@@ -93,18 +110,28 @@ function getNow() {
                    class="flex items-center gap-2">
                 <RadioButton v-model="log.option.logType"
                              :inputId="logType.name" :value="logType"/>
-                <label :for="logType">{{ logType.name }}</label>
+                <label :for="logType.name">{{ logType.name }}</label>
               </div>
             </div>
           </div>
-          <div class="flex flex-col gap-2">
-            <label for="cctv-select" class="font-bold" >CCTV</label>
-            <Select v-model="log.option.targetCctv" :options="cctv.data"
-                optionLabel="cctv_name"
-                filter
-                placeholder="Select a CCTV"
-                inputId="cctv-select"
-            />
+          <div class="flex w-1/7 flex-col gap-2">
+            <template v-if="log.option.logType.name === LOG_TYPE.GSTREAMER.name">
+              <label for="inferenceId-select" class="font-bold" >InferenceId</label>
+              <Select v-model="log.option.inferenceId" :options="cctv.inferenceIds"
+                      filter
+                      placeholder="Select a inferenceId"
+                      inputId="inferenceId-select"
+              />
+            </template>
+            <template v-else>
+              <label for="cctv-select" class="font-bold" >CCTV</label>
+              <Select v-model="log.option.targetCctv" :options="cctv.data"
+                      optionLabel="cctv_name"
+                      filter
+                      placeholder="Select a CCTV"
+                      inputId="cctv-select"
+              />
+            </template>
           </div>
           <div class="flex flex-col gap-2">
             <label for="log-date-picker" class="font-bold"> 날짜 </label>
@@ -113,7 +140,15 @@ function getNow() {
                 inputId="log-date-picker"
             />
           </div>
-          <Button @click="log.fetch.run()" raised>조회</Button>
+          <div class="flex flex-col gap-2">
+            <div class="h-2/5"></div>
+            <Button @click="log.fetch.run()"
+                    raised
+                    class="font-bold text-lg"
+            >
+              검색
+            </Button>
+          </div>
         </div>
       </template>
     </Card>
