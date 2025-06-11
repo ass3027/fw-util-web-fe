d7c2bb231a4261369da585c240921eb5c4ee24b6
ass3027 <blackfeather227@gmail.com>
2025-06-11 13:38:19 +0900

    [CHORE] 배포 스크립트 추가 및 npm run deploy 추가

---
+++ mount-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+server=lsjin@192.168.10.62
+serverPort=2233
+feDeployPoint=/home/workspace/fw_cctv_log_monitor/fe/dist
+
+echo $serverPort $server:$feDeployPoint
+
+#npm run build
+
+ssh -p $serverPort $server rm -r $feDeployPoint
+
+scp -r -P 2233 ./dist/* $server:$feDeployPoint
