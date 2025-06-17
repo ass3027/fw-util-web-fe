88cbc76ab6ff2b21b245398514b5f54f6ad6768d
ass3027 <blackfeather227@gmail.com>
2025-06-17 15:47:32 +0900

    feat(hls) fix hls api and remove unnecessary lines, using templateRef instead of default ref

--- src/views/RealTimeView.vue
+++ src/views/RealTimeView.vue
@@ -1,41 +1,63 @@
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue';
+import {onMounted, onUnmounted, useTemplateRef} from 'vue';
+import { useRoute } from 'vue-router';
 import Hls from 'hls.js';
+import { API } from "@/util/API.js";
 
-const videoPlayer = ref(null);
-const hls = ref(null);
-const hlsSource = ref('https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'); // Example HLS stream
+const route = useRoute();
+const inferenceId = route.query.inferenceId || 'svr10';
+const cctvId = route.query.cctvId || '10';
 
-onMounted(() => {
-  if (Hls.isSupported()) {
-    hls.value = new Hls();
-    hls.value.loadSource(hlsSource.value);
-    hls.value.attachMedia(videoPlayer.value);
-    hls.value.on(Hls.Events.MANIFEST_PARSED, () => {
-      videoPlayer.value.play();
-    });
-  } else if (videoPlayer.value.canPlayType('application/vnd.apple.mpegurl')) {
-    videoPlayer.value.src = hlsSource.value;
-    videoPlayer.value.addEventListener('loadedmetadata', () => {
-      videoPlayer.value.play();
-    });
-  }
-});
+const videoPlayerRef = useTemplateRef("videoPlayer");
+
+let hls = null;
+
+onMounted(async () => initHls());
 
 onUnmounted(() => {
-  if (hls.value) {
-    hls.value.destroy();
+  if (hls) {
+    hls.destroy();
   }
+  API.post("/hls_tunnel_stop");
 });
+
+async function fetchHlsSource() {
+  try {
+    const res = await API.post("/hls_tunnel", {
+      inference_id: inferenceId,
+      cctv_id: cctvId,
+    });
+    return res.data.message;
+  } catch (error) {
+    console.error("Error fetching HLS source:", error);
+    throw error;
+  }
+}
+
+async function initHls() {
+  if (!Hls.isSupported())
+    throw new Error("Hls is not supported");
+
+  const hlsSourceUrl = await fetchHlsSource();
+
+  hls = new Hls();
+  hls.loadSource(hlsSourceUrl);
+  hls.attachMedia(videoPlayerRef.value);
+  hls.on(Hls.Events.MANIFEST_PARSED, () => videoPlayerRef.value.play());
+}
 </script>
+
 <template>
   <Card class="flex-1 min-h-0" 
     pt:content:class="flex flex-1 min-h-0"
     pt:body:class="flex flex-1 min-h-0" 
   >
+    <template #title>
+      <span class="font-bold text-2xl">RealTimeView</span>
+    </template>
     <template #content>
-      <div class="flex flex-1 min-h-0 justify-center items-center bg-black">
-        <video ref="videoPlayer" controls class="flex-1 h-full object-fill" autoplay/>
+      <div class="flex flex-1 min-h-0">
+        <video ref="videoPlayer" controls class="flex-1 h-full" autoplay/>
       </div>
     </template>
   </Card>
