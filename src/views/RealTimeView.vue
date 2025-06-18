d3f99b92ee158d81f3ce49688629a730f236f7da
ass3027 <blackfeather227@gmail.com>
2025-06-18 17:28:41 +0900

    feat(Realtime) change ref cctvId to cctv object and add additional info

--- src/views/RealTimeView.vue
+++ src/views/RealTimeView.vue
@@ -1,32 +1,32 @@
 <script setup>
-import {onMounted, onUnmounted, reactive, ref, useTemplateRef} from 'vue';
+import {onMounted, onUnmounted, reactive, useTemplateRef} from 'vue';
 import { useRoute } from 'vue-router';
 import Hls from 'hls.js';
 import { API } from "@/util/API.js";
 
 const videoPlayerRef = useTemplateRef("videoPlayer");
-
 const route = useRoute();
-const cctvId = ref(route.query.cctvId);
-
-
 let hls = null;
 
 onMounted(async () => {
   await cctv.fetch();
-  if(cctvId.value)
-    await initHls();
+  const queryCctvId = route.query.cctvId;
+  if(queryCctvId !== "")
+    cctv.target = cctv.data.filter(it => it['cctv_ID'] === Number(queryCctvId))[0];
+
+  if(cctv.target !== undefined)
+    await loadHls();
 });
 
 onUnmounted(() => {
-  if (hls) {
+  if (hls)
     hls.destroy();
-  }
+
   API.post("/hls_tunnel_stop");
 });
 
 
-async function initHls() {
+async function loadHls() {
   if (!Hls.isSupported())
     throw new Error("Hls is not supported");
 
@@ -39,11 +39,10 @@ async function initHls() {
 }
 
 async function fetchHlsSource() {
-  const targetCctv = cctv.data.filter(it => it['cctv_ID'] === Number(cctvId.value))[0];
   try {
     const res = await API.post("/hls_tunnel", {
-      cctv_id: cctvId.value,
-      inference_id: targetCctv['inference_id'],
+      cctv_id: cctv.target['cctv_ID'],
+      inference_id: cctv.target['inference_id'],
     });
     return res.data.message;
   } catch (error) {
@@ -54,6 +53,7 @@ async function fetchHlsSource() {
 
 const cctv = reactive({
   data: [],
+  target: undefined,
   async fetch() {
     const res = await API.post("/cctv_data")
     const data = res.data.message;
@@ -66,20 +66,65 @@ const cctv = reactive({
 </script>
 
 <template>
-  <Card class="flex-1 min-h-0" 
-    pt:content:class="flex flex-1 min-h-0"
-    pt:body:class="flex flex-1 min-h-0" 
-  >
-    <template #title>
-      <span class="font-bold text-2xl">RealTimeView</span>
-    </template>
-    <template #content>
-      <div class="flex flex-1 min-h-0 rounded-xl border-5 border-surface-100 bg-surface-100">
-        <video ref="videoPlayer" class="flex-1 h-full rounded-xl"
-               autoplay controls muted/>
-      </div>
-    </template>
-  </Card>
+  <div class="flex flex-col gap-6">
+    <span class="font-bold text-xl">RealTimeView</span>
+    <div class="flex gap-4">
+      <Card class="flex w-1/4" pt:body:class="p-4">
+        <template #content>
+          <div class="flex justify-items-start gap-6">
+            <div class="flex flex-col flex-1 justify-items-start gap-2">
+              <label for="cctv-select" class="font-bold" >CCTV</label>
+              <Select v-model="cctv.target" :options="cctv.data"
+                      class="text-base"
+                      filter
+                      option-label="cctv_name"
+                      placeholder="Select a CCTV"
+                      input-id="cctv-select"
+              />
+            </div>
+            <div class="flex flex-col gap-2">
+              <div class="h-2/5"></div>
+              <Button @click="loadHls"
+                      raised
+                      class="font-bold text-lg"
+              >
+                연결
+              </Button>
+            </div>
+          </div>
+        </template>
+      </Card>
+      <Card class="flex flex-1 justify-center" pt:body:class="p-4">
+        <template #content>
+          <div class="flex gap-10 px-2">
+            <div class="flex flex-col gap-2">
+              <span class="font-semibold">분석 서버</span>
+              <span class="h-[1.875rem] text-3xl font-semibold">
+                {{ cctv.target !== undefined ? cctv.target['inference_id'] : "" }}
+              </span>
+            </div>
+            <div class="flex flex-col gap-2">
+              <span class="font-semibold">RTSP URL</span>
+              <span class="h-[1.875rem] text-3xl font-semibold">
+                {{ cctv.target !== undefined ? cctv.target['url'] : "" }}
+              </span>
+            </div>
+          </div>
+        </template>
+      </Card>
+    </div>
+    <Card class="flex-1 min-h-0"
+          pt:content:class="flex flex-1 min-h-0"
+          pt:body:class="flex flex-1 min-h-0"
+    >
+      <template #content>
+        <div class="flex flex-1 min-h-0 rounded-xl border-5 border-surface-100 bg-surface-100">
+          <video ref="videoPlayer" class="flex-1 h-full rounded-xl"
+                 autoplay controls muted/>
+        </div>
+      </template>
+    </Card>
+  </div>
 </template>
 
 <style scoped>
